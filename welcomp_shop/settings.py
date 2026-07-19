8139f87f2b1b3fd6668f565824ad64f0c9101e8a
Javier L. Lopez P. <welcomptecnologia@gmail.com>
2026-07-19 05:56:00 +0000

    ajuste nuevo en settings

--- welcomp_shop/settings.py
+++ welcomp_shop/settings.py
@@ -24,14 +24,6 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 
-CSRF_TRUSTED_ORIGINS = ['https://feed.welcomp.net', 'https://ingeniarte.co', 'https://feed.ingeniarte.co', 'http://127.0.0.1:8001', 'http://localhost:8001']
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-CSRF_COOKIE_SECURE = True
-
-SESSION_COOKIE_SECURE = True
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
