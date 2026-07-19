0134122fb19cb08693970d1356230681dced357d
Javier L. Lopez P. <welcomptecnologia@gmail.com>
2026-07-19 04:26:22 +0000

    sc

--- welcomp_shop/settings.py
+++ welcomp_shop/settings.py
@@ -24,9 +24,8 @@ DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 
-CSRF_TRUSTED_ORIGINS = ['https://feed.welcomp.net', 'https://ingeniarte.co', 'http://127.0.0.1:8001', http://localhost:8001]
-'http://127.0.0.1:8000',
-    'http://localhost:8000',
+CSRF_TRUSTED_ORIGINS = ['https://feed.welcomp.net', 'https://ingeniarte.co', 'http://127.0.0.1:8001', 'http://localhost:8001']
+
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 CSRF_COOKIE_SECURE = True
